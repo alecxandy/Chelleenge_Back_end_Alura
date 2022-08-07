c14d36be288b42d198bab4e53e7b4d8e4e3a3cde
alexandre gomes <alecxandy@gmail.com>
2022-08-07 20:31:35 -0300

    Dependencias

---
+++ src/test/java/br/com/alexandre/Desafio/Back/end/DesafioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexandre.Desafio.Back.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
